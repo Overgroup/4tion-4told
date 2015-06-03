764d59cb916f8ff47f94e7b5b3818824415228d2
Ross Overstreet <rosso@overgroup.com>
2015-06-03 11:09:22 -0400

    Fix twilio receive

--- routes/twilio-receive.js
+++ routes/twilio-receive.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var helper = require('../lib/helper');
 router.get('/', function(req, res) {
-    helper.processIncomingText(req.query.From, req.query.message);
+    helper.processIncomingText(req.query.From, req.query.Body);
     res.send('');
 });
 module.exports = router;
